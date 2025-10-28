e16627c89d48a8d6083b40f1c9d9266a8d9f7965
Abhiraj-Sagar <abhiraj.sagar@soniconsultancy.in>
2025-10-28 16:48:01 +0530

    Made landing page responsive

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,17 @@ import { useState } from "react";
 
 import Navbar from "@/components/Navbar";
 import Tabs from "@/components/HomePageTabs"
+import FallingGraphics from "@/components/FallingGraphics";
 
 export default function Home() 
 {
   return (
     <>
       <Navbar/>
-      <div className="w-full flex justify-end px-12 flex-col">
-        <h2 className="bg-gradient-to-r mt-[12vh] from-amber-400 to-orange-600 font-extrabold text-6xl text-transparent bg-clip-text w-fit">Hey there,</h2>
-        <h2 className="text-muted font-extrabold text-4xl">Welcome to Code Collab</h2>
-        <TabsComponent/>    
+      <div className="w-full flex justify-center items-center md:items-start px-12 flex-col">
+        <h2 className="bg-gradient-to-r mt-[12vh] from-amber-400 to-orange-600 font-extrabold text-4xl sm:text-6xl text-transparent bg-clip-text w-fit">Hey there,</h2>
+        <h2 className="text-muted font-extrabold text-xl sm:text-4xl">Welcome to Code Collab</h2>
+        <TabsComponent/>  
       </div>
     </>
   );
@@ -24,10 +25,10 @@ function TabsComponent()
 
   return (
     <>
-      <div className="w-full h-10 mt-15 flex">
-        {Tabs.map((tab, idx) => <h2 key={idx} onClick={() => setCurrentTab(tab)} className={`w-fit mx-3 cursor-pointer ${tab === currentTab ? 'text-orange-400 underline' : 'text-muted'}`}>{tab.name}</h2>)}
+      <div className="w-full h-10 mt-15 flex justify-center md:justify-start">
+        {Tabs.map((tab, idx) => <h2 key={idx} onClick={() => setCurrentTab(tab)} className={`w-fit mx-3 text-sm sm:text-lg text-nowrap cursor-pointer ${tab === currentTab ? 'text-orange-400 underline' : 'text-muted'}`}>{tab.name}</h2>)}
       </div>
-      <div className="dark:bg-background-muted-dark bg-white h-max w-1/2 rounded-md">
+      <div className="dark:bg-background-muted-dark bg-white h-max w-3/4 sm:w-1/2 min-w-72 rounded-md">
         {currentTab.content}
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { faCode } from "@fortawesome/free-solid-svg-icons";
 export default function Navbar()
 {
     return (
-        <div className="w-full h-12 flex justify-space items-center px-5">
+        <div className="w-full h-12 flex justify-center sm:justify-start items-center px-5">
             <div className="flex flex-row justify-center items-center gap-2">
                 <FontAwesomeIcon icon={faCode} className="text-3xl text-amber-600 font-extrabold" />
                 <h2 className="text-muted font-extrabold text-lg">Code Collab</h2>
